ab565ab2fb13fa5dc99cf111c544f1d8087de75a
Smooth <1057213071@qq.com>
2025-07-09 01:39:15 +0800

    🔧 chore: 更新WATCH_FILE常量，使用CONFIG_FILE替代，在文件监视器准备就绪时添加日志输出

--- packages/core/lib/constants/index.ts
+++ packages/core/lib/constants/index.ts
@@ -3,4 +3,4 @@ export const OUTPUT_DIR = "output";
 // 配置文件名
 export const CONFIG_FILE = "generate.config.json";
 // 监听的文件以及目录
-export const WATCH_FILE = ["generate.config.json", "config", "template"];
+export const WATCH_FILE = [CONFIG_FILE, "config", "template"];
--- packages/core/lib/devServe/watch.ts
+++ packages/core/lib/devServe/watch.ts
@@ -1,5 +1,6 @@
 import chokidar from "chokidar";
 import { WATCH_FILE } from "@core/constants";
+import logger from "@core/logging";
 
 export const watch = (cb: (event: string, path: string) => void) => {
   const watcher = chokidar.watch(WATCH_FILE, {
@@ -9,7 +10,13 @@ export const watch = (cb: (event: string, path: string) => void) => {
     // 初始化时不触发事件
     ignoreInitial: true,
   });
+  watcher.on("ready", () => {
+    logger.info(`开发服务启动成功，监听文件变化`);
+  });
   watcher.on("all", (event, path) => {
-    cb(event, path);
+    const cbEvent = ["change", "add", "addDir", "unlink", "unlinkDir"];
+    if (cbEvent.includes(event)) {
+      cb(event, path);
+    }
   });
 };
